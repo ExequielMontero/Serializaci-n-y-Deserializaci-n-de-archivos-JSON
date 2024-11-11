ab3c1ced34bf4c299ef59b5fdda39a47780df454
ExequielMontero <exeee2015@gmail.com>
2024-11-11 13:25:39 -0300

    En proceso

---
+++ Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializar_y_Deserializar_archivos_Json
+{
+    internal class Persona
+    {
+        public Persona(string nombre, string apellido, string email, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+            Edad = edad;
+        }
+
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+
+        public string Email { get; private set; }
+        public int Edad { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Apellido: {Apellido}, Email: {Email}, Edad: {Edad}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Serializar_y_Deserializar_archivos_Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serializacion de un Objeto a Json
+            
+            /*Persona actual = new Persona("Exequiel", "Montero", "exequielalejandromontero@gmail.com", 21);
+            string MiJson = JsonSerializer.Serialize(actual);
+            File.WriteAllText("objeto.txt",MiJson);*/
+
+
+            
+            //Deserializacion de un Json a objeto
+            string MiJsonDes = File.ReadAllText("objeto.txt");
+            Persona persona = JsonSerializer.Deserialize<Persona>(MiJsonDes);
+            Console.WriteLine(persona.ToString());
+        }
+    }
+}
